ecd32e0a02ba9e9580c3974eb05b4f319b6a2681
Lucian Stoian <lucians@ENSRO0414.local>
2026-02-19 09:54:59 +0200

    update paths

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -3,16 +3,17 @@ import type { RegisterCredentials } from "../pages/RegisterPage";
 import { api } from "./axiosConfig";
 
 const enum USER_ROUTES {
-    REGISTER = "/users",
+    CREATE_USER = "/users",
 }
 
 export async function register(credentials: RegisterCredentials) {
 
     try {
-        const response = await api.post(USER_ROUTES.REGISTER, credentials);
-        return response.data;
-    } catch (error) {
-        console.log((error as AxiosError).response);
-        throw error;
+        const response = await api.post(USER_ROUTES.CREATE_USER, credentials);
+
+        return response.data
+    } catch (error: any) {
+        console.log(error.response)
+        throw error
     }
 }

e71750fe61a24554f4bf4482a406206a0985df99
lucians <lucians@ensemble.com>
2026-08-11 17:08:32 +0300

    asdasd

--- src/components/ChatEditor/ChatEditor.tsx
+++ src/components/ChatEditor/ChatEditor.tsx
@@ -9,6 +9,7 @@ type ChatEditorProps<T> = {
 }
 
 export default function ChatEditor<T>({ onChange, items, displayKey }: ChatEditorProps<T>) {
+    asdasdadsasd
     const editor = useEditor({
         extensions: [
             StarterKit,

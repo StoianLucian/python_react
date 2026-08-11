69fcf3ec7bfe517b36ca235054d0fe4f6181c731
lucians <lucians@ensemble.com>
2026-08-11 16:12:35 +0300

    tesst

--- src/components/ChatEditor/ChatEditor.tsx
+++ src/components/ChatEditor/ChatEditor.tsx
@@ -9,6 +9,7 @@ type ChatEditorProps<T> = {
 }
 
 export default function ChatEditor<T>({ onChange, items, displayKey }: ChatEditorProps<T>) {
+    "testing"
     const editor = useEditor({
         extensions: [
             StarterKit,

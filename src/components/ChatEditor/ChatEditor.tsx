1f4bd49d75d716549bc4c2da3bd1b5ac05ea1c9b
lucians <lucians@ensemble.com>
2026-08-11 16:35:16 +0300

    tesst

--- src/components/ChatEditor/ChatEditor.tsx
+++ src/components/ChatEditor/ChatEditor.tsx
@@ -9,6 +9,9 @@ type ChatEditorProps<T> = {
 }
 
 export default function ChatEditor<T>({ onChange, items, displayKey }: ChatEditorProps<T>) {
+    console.log(items);
+
+    "aaadga"
     const editor = useEditor({
         extensions: [
             StarterKit,
